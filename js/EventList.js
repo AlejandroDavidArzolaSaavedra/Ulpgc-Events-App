ed0c2379c3845dfa5416291a80f5a506792fe4c1
yabpenserio <yabpenserio@gmail.com>
2023-04-25 09:43:48 +0100

    Cambios EventList.JS

--- js/EventList.js
+++ js/EventList.js
@@ -1,3 +1,4 @@
+
 async function getEvents() {
     let url = '../staticData/events.json'
     try {
@@ -9,16 +10,21 @@ async function getEvents() {
 }
 async function renderEvents() {
     var tableEvent = document.getElementById("table-event-list");
-    const data = await getEvents();
+    const data = await getEvents(); 
     let tr = ""
     data.eventos.forEach(evento =>{
+        var month = getMonthString(evento.fecha.mes);
         tr += "<tr>";
         tr += `<td>${evento.nombreEvento}</td>`;
-        tr += `<td>${evento.fecha.dia},${evento.fecha.mes},${evento.fecha.anio}</td>`;
+        tr += `<td>${evento.fecha.dia},${month},${evento.fecha.anio}</td>`;
         tr += `<td>${evento.tipoEvento}</td>`;
-        tr += `<td>${evento.aforo}</td>`;
+        if(compareDate(evento.fecha.anio, evento.fecha.mes, evento.fecha.dia)){
+            tr += `<td class="text-success date-field">En Fecha</td>`;
+        }else{
+            tr += `<td class="text-danger date-field">Fuera de Fecha</td>`;
+        }
         tr += `<td style="text-align: center;" class="no-export">
-                    <button class="btn btn-success" type="submit">
+                    <button class="btn btn-success" type="submit" onclick="userEvents(${evento})">
                         <span class="btn-label"></span>
                         <i class="far fa-edit"></i>Inscripciones
                     </button> 
@@ -45,4 +51,16 @@ function exportCSVExcel() {
         exclude_inputs: true,
         preserveColors: true
     });
+}
+function compareDate(day,month,year){
+
+    var actualDate = new Date();
+    var reservedDate = new Date(day, month-1, year)
+    return reservedDate.getTime() > actualDate.getTime()
+    
+}
+function getMonthString(index){
+    var enMonthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
+    var esMonthNames = ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dic"];
+    return esMonthNames[index-1];  
 }
