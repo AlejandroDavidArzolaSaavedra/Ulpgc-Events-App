c34119e98e4c6aacc45a7e38ddcb3c15e4557654
AlejandroDavidArzolaSaavedra <alejandro.arzola101@alu.ulpgc.es>
2023-04-30 02:34:31 +0100

    4 change of Sprint 1

---
+++ js/modificarPerfil.js
@@ -0,0 +1,92 @@
+import {db } from "../js/firebase.js";
+import {updateDoc, getDocs, collection } from "https://www.gstatic.com/firebasejs/9.19.0/firebase-firestore.js"
+
+let originalNombre = '';
+let originalCorreo = '';
+let originalTelefono = '';
+let originalContraseña = '';
+
+export const changeInfoUserSave = (usuarioRegistrado) => {
+    const usuarioAlmacenado = localStorage.getItem('usuario');
+    const usuario = JSON.parse(usuarioAlmacenado);
+    
+    document.getElementById("cambiarNombreUsuario").value = usuarioRegistrado.nombre;
+    document.getElementById("cambiarCorreoUsuario").value = usuarioRegistrado.correo;
+    document.getElementById("cambiarTelefonoUsuario").value = usuarioRegistrado.telefono;
+    document.getElementById("cambiarContraseñaUsuario").value = usuarioRegistrado.contra;
+       
+    const modificarBoton = document.getElementById("BotonModificarUsuario");
+    console.log(modificarBoton);
+    modificarBoton.addEventListener("click", async () => {
+
+        const nombre = document.getElementById("cambiarNombreUsuario").value;
+        const correo = document.getElementById("cambiarCorreoUsuario").value;
+        const telefono = document.getElementById("cambiarTelefonoUsuario").value;
+        const contraseña = document.getElementById("cambiarContraseñaUsuario").value;
+        console.log(nombre);
+        console.log(correo);
+        console.log(telefono);
+        console.log(contraseña);
+        const userDocRef = await getDocs(collection(db, "users"));
+        
+       
+        console.log(userDocRef,"ASDASDSDSDADASDASDASD");
+         await updateDoc(userDocRef, {
+          nombre: nombre,
+          correo: correo,
+          telefono: telefono,
+          contraseña: contraseña
+        });
+    });
+    const botonEliminarCambios = document.getElementById('BotonEliminarCambios');
+    botonEliminarCambios.addEventListener('click', async () => {
+   
+        document.getElementById('cambiarNombreUsuario').value = originalNombre;
+        document.getElementById('cambiarCorreoUsuario').value = originalCorreo;
+        document.getElementById('cambiarTelefonoUsuario').value = originalTelefono;
+        document.getElementById('cambiarContraseñaUsuario').value = originalContraseña;
+    });
+    
+  }
+
+
+/////////////////////////////////////////////////////////////
+//                  INFORMACION DEL PERFIL
+/////////////////////////////////////////////////////////////
+
+  export const changeInfoUser = (usuarioRegistrado) => {
+    const Nombre = "isai@gmail.com";
+    const usuarioAlmacenado = localStorage.getItem('usuario');
+    const usuario = JSON.parse(usuarioAlmacenado);
+    console.log(usuario.contra);
+    
+    document.getElementById("NombreInformacionUsuario").value = usuarioRegistrado.nombre;
+    document.getElementById("CorreoInformacionUsuario").value = usuarioRegistrado.correo;
+    document.getElementById("TelefonoInformacionUsuario").value = usuarioRegistrado.telefono;        
+    document.getElementById("ContraseñaInformacionUsuario").value = usuarioRegistrado.contra;
+    }
+
+
+
+  const buttonEdit = document.querySelector("#button-edit-perfil");
+    const buttonSave = document.querySelector("#button-save-perfil");
+    const buttonDeleteChanges = document.querySelector("#BotonEliminarCambios");
+    if(buttonEdit){
+    buttonEdit.addEventListener('click', function() {
+        const buttonEd = document.querySelectorAll(".hidden");
+        buttonEd.forEach( buttons => buttons.style.display = "inline-block");
+        buttonEdit.style.display = "none"
+
+    });}
+
+ 
+if(buttonDeleteChanges){
+    buttonDeleteChanges.addEventListener('click', function() {
+        const buttonEd = document.querySelectorAll(".hidden");
+        buttonEd.forEach( buttons => buttons.style.display = "none");
+        buttonEdit.style.display = "inline-block"
+        
+    });}
+
+
+    
